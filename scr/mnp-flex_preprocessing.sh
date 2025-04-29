f71ee0c408b4f4d5d195f0dfb5df2bd8581c3717
Ryan SY Kwan <guanhomer@gmail.com>
2025-04-29 09:16:27 +0200

    combine lines with pipe for slow I/O

--- scr/mnp-flex_preprocessing.sh
+++ scr/mnp-flex_preprocessing.sh
@@ -10,34 +10,28 @@ ID=$4  # ID as output filename
 mkdir -p "$OUT_PATH"
 
 # Filter for m (5mC) rows from bedMethyl file to prevent duplicated rows
-awk '$4 == "m"' "$IN_FILE" > "${OUT_PATH}/${ID}.tmp1.bed"
-
 # Intersect with the reference file for IlmnID using bedtools
-bedtools intersect -a "${OUT_PATH}/${ID}.tmp.bed" -b "$MNP_BED" -wa -wb > "${OUT_PATH}/${ID}.tmp2.bed"
-
-# Add column names to the output file
-column_names="chr start end coverage methylation_percentage IlmnID"
-echo -e "$column_names" > "${OUT_PATH}/${ID}.MNPFlex.subset.bed"
-
-# Sleep 5 seconds to ensure the file is written before appending
-sleep 5
+awk '$4 == "m"' "$IN_FILE" | \
+bedtools intersect -a stdin -b "$MNP_BED" -wa -wb > "${OUT_PATH}/${ID}.tmp.bed"
 
 # Group by IlmnID (column $25) and summarize (sum) columns N_valid-cov ($10), N_mod ($12), and N_other-mod ($14)
 # Methylation rate = ( N_mod + N_other-mod ) / N_valid-cov
-awk -v FS="\t" -v OFS=" " '{
-    coverage[$25] += $10; 
-    modC[$25] += $12 + $14; 
-    chr[$25] = $19; 
-    start[$25] = $20; 
-    end[$25] = $21
-} 
-END {
-    for (id in coverage) {
-        score = modC[id] / coverage[id] * 100
-        # Print the result with space delimiters and 2 decimal places for the score
-        printf "%s %s %s %d %.2f %s\n", chr[id], start[id], end[id], coverage[id], score, id
-    }
-}' "${OUT_PATH}/${ID}.tmp.bed" >> "${OUT_PATH}/${ID}.MNPFlex.subset.bed"
+# Create the output file with header and appended summary in one go
+{
+    echo "chr start end coverage methylation_percentage IlmnID"
+    awk -v FS="\t" -v OFS=" " '{
+        coverage[$25] += $10; 
+        modC[$25] += $12 + $14; 
+        chr[$25] = $19; 
+        start[$25] = $20; 
+        end[$25] = $21
+    } 
+    END {
+        for (id in coverage) {
+            score = modC[id] / coverage[id] * 100
+            printf "%s %s %s %d %.2f %s\n", chr[id], start[id], end[id], coverage[id], score, id
+        }
+    }' "${OUT_PATH}/${FILENAME}.tmp.bed"
+} > "${OUT_PATH}/${FILENAME}.MNPFlex.subset.bed"
 
-rm -r ${OUT_PATH}/${ID}.tmp1.bed
-rm -r ${OUT_PATH}/${ID}.tmp2.bed
+rm ${OUT_PATH}/${ID}.tmp.bed
